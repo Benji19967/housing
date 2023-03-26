525d571b8104b469a0c6e1afed340adb10bab8f8
Benjamin Labrecque <labrecque.benji@gmail.com>
2023-03-26 17:29:25 +0200

    Add comments

--- housing/homegate.py
+++ housing/homegate.py
@@ -2,14 +2,15 @@ import csv
 import datetime
 import pathlib
 from typing import Any, Iterable, List
-from housing.config import settings
 
 import pendulum
 import requests
 from bs4 import BeautifulSoup
-from pydantic import BaseModel
 from pendulum import parse as pendulum_parse  # type: ignore
 from pendulum.datetime import DateTime
+from pydantic import BaseModel
+
+from housing.config import settings
 
 RADIUS_METERS = 2000
 LOCATION = "city-zug"
@@ -19,12 +20,20 @@ CSV_FILEPATH = settings.HOMEGATE_CSV_FILEPATH
 
 
 def parse_datetime(dt: str) -> DateTime:
+    """
+    Convert ISO string datetimes to pendulum DateTimes
+    """
     timestamp = pendulum_parse(dt)
     assert isinstance(timestamp, DateTime)
     return timestamp
 
 
 class Listing(BaseModel):
+    """
+    Each listing has a unique identifier number.
+
+    The listings can be accessed via "homegate.ch/rent/<id>"
+    """
     id: int
 
     class Config:
@@ -32,6 +41,10 @@ class Listing(BaseModel):
 
 
 class Row(BaseModel):
+    """
+    The results of a run get stored as a new row in a file
+    if there are new listings.
+    """
     timestamp: datetime.datetime
     num_listings: int
     listings: List[Listing]
@@ -55,6 +68,11 @@ class Row(BaseModel):
 
 
 def read_rows(filename: str) -> List[Row]:
+    """
+    Read rows from log file.
+
+    Log file is in CSV format.
+    """
     file = pathlib.Path(filename)
     if not file.exists():
         return []
@@ -65,12 +83,24 @@ def read_rows(filename: str) -> List[Row]:
 
 
 def write_row(filename: str, row: Row) -> None:
+    """
+    Write rows to file in CSV format
+    """
     with open(filename, "a", newline="") as csvfile:
         writer = csv.writer(csvfile, delimiter=",")
         writer.writerow(row.to_csv())
 
 
 def fetch_listings() -> List[Listing]:
+    """
+    Fetch listings from HTML page.
+
+    The "TOP" listings are stored differently than
+    regular listings.
+
+    Each page contains at most 20 listings, hence we 
+    fetch multiple pages until we get to a page with no listings.
+    """
     listings: List[Listing] = []
 
     for page_number in range(1, 10):
@@ -99,6 +129,10 @@ def fetch_listings() -> List[Listing]:
 
 
 def get_new_listings(new_row: Row, previous_row: Row) -> List[Listing]:
+    """
+    Check if the listings we fetched contains new listings
+    compared to the listings we fetched in the last run.
+    """
     new_listings = set(new_row.listings) - set(previous_row.listings)
     return list(new_listings)
 
@@ -111,7 +145,9 @@ def main() -> None:
     listings = fetch_listings()
 
     new_row = Row(
-        timestamp=pendulum.now(), num_listings=len(listings), listings=listings
+        timestamp=pendulum.now(),
+        num_listings=len(listings),
+        listings=listings,
     )
 
     if not current_rows:
