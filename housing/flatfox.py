fd5a88e1cccfe89244f370febbce16287f36dabe
Benjamin Labrecque <labrecque.benji@gmail.com>
2023-11-04 22:24:52 -0300

    Format flatfox utils

--- housing/flatfox.py
+++ housing/flatfox.py
@@ -1,16 +1,20 @@
-from selenium.webdriver import Chrome
+# from selenium.webdriver import Chrome
 from bs4 import BeautifulSoup
-from selenium.webdriver.chrome.options import Options
 
-chrome_options = Options()
+# from selenium.webdriver.chrome.options import Options
+
+# chrome_options = Options()
 # chrome_options.add_argument("--disable-extensions")
 # chrome_options.add_argument("--disable-gpu")
 # chrome_options.add_argument("--no-sandbox") # linux only
-chrome_options.add_argument("--headless=new")  # for Chrome >= 109
+# chrome_options.add_argument("--headless=new")  # for Chrome >= 109
 # chrome_options.add_argument("--headless")
 
+import selenium_utils
+
 if __name__ == "__main__":
-    driver = Chrome(options=chrome_options)
+    # driver = Chrome(options=chrome_options)
+    driver = selenium_utils.driverInit()
 
     driver.get(
         "https://flatfox.ch/en/search/?east=8.680391&north=47.421354&query=Z%C3%BCrich&south=47.333591&west=8.392925"
