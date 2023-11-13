34513a71a5ccc9c3fef305558abd45f6fe038e58
Benjamin Labrecque <labrecque.benji@gmail.com>
2023-11-13 19:41:02 -0300

    Clean up flatfox, get listing from public api

--- housing/flatfox.py
+++ housing/flatfox.py
@@ -1,25 +1,5 @@
-# from selenium.webdriver import Chrome
-from bs4 import BeautifulSoup
-
-# from selenium.webdriver.chrome.options import Options
-
-# chrome_options = Options()
-# chrome_options.add_argument("--disable-extensions")
-# chrome_options.add_argument("--disable-gpu")
-# chrome_options.add_argument("--no-sandbox") # linux only
-# chrome_options.add_argument("--headless=new")  # for Chrome >= 109
-# chrome_options.add_argument("--headless")
-
-import selenium_utils
+import requests
 
 if __name__ == "__main__":
-    # driver = Chrome(options=chrome_options)
-    driver = selenium_utils.driverInit()
-
-    driver.get(
-        "https://flatfox.ch/en/search/?east=8.680391&north=47.421354&query=Z%C3%BCrich&south=47.333591&west=8.392925"
-    )
-
-    soup = BeautifulSoup(driver.page_source, "lxml")
-    for listing_id in soup.find_all("a", class_="listing-thumb-title"):
-        print(listing_id)
+    result = requests.get("https://flatfox.ch/api/v1/public-listing/")
+    print(result.json())
